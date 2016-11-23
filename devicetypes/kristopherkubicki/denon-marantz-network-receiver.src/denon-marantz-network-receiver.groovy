bec967499c1225f928a808c972ef1eb3d77598a2
Kyle Davis <kyle.davis@charter.com>
2016-11-23 11:49:19 -0500

    Updated code formatting.

--- devicetypes/kristopherkubicki/denon-marantz-network-receiver.src/denon-marantz-network-receiver.groovy
+++ devicetypes/kristopherkubicki/denon-marantz-network-receiver.src/denon-marantz-network-receiver.groovy
@@ -1,4 +1,3 @@
-
 preferences {
     //the IP address of the receiver
     input("destIp", "text", title: "IP", description: "The device IP")
@@ -7,251 +6,254 @@ preferences {
 }
 
 metadata {
-	definition (name: "Denon / Marantz Network Receiver", namespace: "KristopherKubicki", 
-    	author: "kristopher@acm.org") {
+    definition(name: "Denon / Marantz Network Receiver", namespace: "KristopherKubicki",
+            author: "kristopher@acm.org") {
         //the Actuator capability just indicates to SmartThings that this device has commands
         capability "Actuator"
         //Switch to turn the receiver on and off
-        capability "Switch" 
+        capability "Switch"
         //Polling to retrieve
         capability "Polling"
         //Switch level to control volume
         capability "Switch Level"
-        
+
         //mute attribute
         attribute "mute", "string"
         //the current input
         attribute "input", "string"
         //the current input channel
         attribute "inputChan", "enum"
-        
+
         command "mute"
         command "unmute"
         command "inputSelect", ["string"] //define that inputSelect takes a string of the input name as a parameter
         command "inputNext"
         command "toggleMute"
-        
-      	}
 
-	simulator {
-		// TODO-: define status and reply messages here
-	}
+    }
 
-	tiles {
-    	//the device power status
-		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: false, canChangeBackground: true) {
-            state "on", label: '${name}', action:"switch.off", backgroundColor: "#79b821", icon:"st.Electronics.electronics16"
-            state "off", label: '${name}', action:"switch.on", backgroundColor: "#ffffff", icon:"st.Electronics.electronics16"
+    simulator {
+        // TODO-: define status and reply messages here
+    }
+
+    tiles {
+        //the device power status
+        standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: false, canChangeBackground: true) {
+            state "on", label: '${name}', action: "switch.off", backgroundColor: "#79b821", icon: "st.Electronics.electronics16"
+            state "off", label: '${name}', action: "switch.on", backgroundColor: "#ffffff", icon: "st.Electronics.electronics16"
         }
         //the current receiver input
         standardTile("input", "device.input", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
-			state "input", label: '${currentValue}', action: "inputNext", icon: "", backgroundColor: "#FFFFFF"
-		}
+            state "input", label: '${currentValue}', action: "inputNext", icon: "", backgroundColor: "#FFFFFF"
+        }
         //muted indicator
         standardTile("mute", "device.mute", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
-            state "muted", label: '${name}', action:"unmute", backgroundColor: "#79b821", icon:"st.Electronics.electronics13"
-            state "unmuted", label: '${name}', action:"mute", backgroundColor: "#ffffff", icon:"st.Electronics.electronics13"
-		}
+            state "muted", label: '${name}', action: "unmute", backgroundColor: "#79b821", icon: "st.Electronics.electronics13"
+            state "unmuted", label: '${name}', action: "mute", backgroundColor: "#ffffff", icon: "st.Electronics.electronics13"
+        }
         //volume
         controlTile("level", "device.level", "slider", height: 1, width: 2, inactiveLabel: false, range: "(0..100)") {
-			state "level", label: '${name}', action:"setLevel"
-		}
+            state "level", label: '${name}', action: "setLevel"
+        }
         //button to refresh all device data
-		standardTile("poll", "device.poll", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
-			state "poll", label: "", action: "polling.poll", icon: "st.secondary.refresh", backgroundColor: "#FFFFFF"
-		}
-        
-		main "switch"
-        details(["switch","input","mute","level","poll"])
-	}
+        standardTile("poll", "device.poll", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
+            state "poll", label: "", action: "polling.poll", icon: "st.secondary.refresh", backgroundColor: "#FFFFFF"
+        }
+
+        main "switch"
+        details(["switch", "input", "mute", "level", "poll"])
+    }
 }
 
 def parse(String description) {
-	log.debug "Parsing '${description}'"
-    
- 	def map = stringToMap(description)
-    
-    if(!map.body || map.body == "DQo=") { return }
-        log.debug "${map.body} "
-	def body = new String(map.body.decodeBase64())
-    
-	def statusrsp = new XmlSlurper().parseText(body)
-	def power = statusrsp.Power.value.text()
-    if(power == "ON") { 
-    	sendEvent(name: "switch", value: 'on')
+    log.debug "Parsing '${description}'"
+
+    def map = stringToMap(description)
+
+    if (!map.body || map.body == "DQo=") {
+        return
     }
-    if(power != "" && power != "ON") { 
-    	sendEvent(name: "switch", value: 'off')
+    log.debug "${map.body} "
+    def body = new String(map.body.decodeBase64())
+
+    def statusrsp = new XmlSlurper().parseText(body)
+    def power = statusrsp.Power.value.text()
+    if (power == "ON") {
+        sendEvent(name: "switch", value: 'on')
     }
-    
+    if (power != "" && power != "ON") {
+        sendEvent(name: "switch", value: 'off')
+    }
+
 
     def muteLevel = statusrsp.Mute.value.text()
-    if(muteLevel == "on") { 
-    	sendEvent(name: "mute", value: 'muted')
-	}
-    if(muteLevel != "" && muteLevel != "on") {
-	    sendEvent(name: "mute", value: 'unmuted')
+    if (muteLevel == "on") {
+        sendEvent(name: "mute", value: 'muted')
     }
-    
-	def inputCanonical = statusrsp.InputFuncSelect.value.text()
+    if (muteLevel != "" && muteLevel != "on") {
+        sendEvent(name: "mute", value: 'unmuted')
+    }
+
+    def inputCanonical = statusrsp.InputFuncSelect.value.text()
     def inputTmp = []
     //check to see if the VideoSelectLists node is available
-    if(!statusrsp.VideoSelectLists.isEmpty()){
-    	log.debug "VideoSelectLists is available... parsing"
+    if (!statusrsp.VideoSelectLists.isEmpty()) {
+        log.debug "VideoSelectLists is available... parsing"
         statusrsp.VideoSelectLists.value.each {
-        	log.debug "$it"
-            if(it.@index != "ON" && it.@index != "OFF") {
+            log.debug "$it"
+            if (it.@index != "ON" && it.@index != "OFF") {
                 inputTmp.push(it.'@index')
                 log.debug "Adding Input ${it.@index}"
-                if(it.toString().trim() == inputCanonical) {     
+                if (it.toString().trim() == inputCanonical) {
                     sendEvent(name: "input", value: it.'@index')
                 }
             }
         }
     }
     //if the VideoSelectLists node is not available, let's try the InputFuncList
-    else if(!statusrsp.InputFuncList.isEmpty()){
-    	log.debug "InputFuncList is available... parsing"
+    else if (!statusrsp.InputFuncList.isEmpty()) {
+        log.debug "InputFuncList is available... parsing"
         statusrsp.InputFuncList.value.each {
-            if(it != "ON" && it != "OFF") {
+            if (it != "ON" && it != "OFF") {
                 inputTmp.push(it)
                 //log.debug "Adding Input ${it}"
-                if(it.toString().trim() == inputCanonical) {     
+                if (it.toString().trim() == inputCanonical) {
                     sendEvent(name: "input", value: it)
                 }
             }
         }
     }
-    
+
     sendEvent(name: "inputChan", value: inputTmp)
-    
-    if(statusrsp.MasterVolume.value.text()) { 
-    	def int volLevel = (int) statusrsp.MasterVolume.value.toFloat() ?: -40.0
+
+    if (statusrsp.MasterVolume.value.text()) {
+        def int volLevel = (int) statusrsp.MasterVolume.value.toFloat() ?: -40.0
         volLevel = (volLevel + 80) * 0.9
-        
-   		def int curLevel = 36
+
+        def int curLevel = 36
         try {
-        	curLevel = device.currentValue("level")
-        } catch(NumberFormatException nfe) { 
-        	curLevel = 36
+            curLevel = device.currentValue("level")
+        } catch (NumberFormatException nfe) {
+            curLevel = 36
         }
-	
-        if(curLevel != volLevel) {
-    		sendEvent(name: "level", value: volLevel)
+
+        if (curLevel != volLevel) {
+            sendEvent(name: "level", value: volLevel)
         }
-    } 
+    }
 }
 
 //set the volume level
 def setLevel(val) {
-	sendEvent(name: "mute", value: "unmuted")     
+    sendEvent(name: "mute", value: "unmuted")
     sendEvent(name: "level", value: val)
-	def int scaledVal = val * 0.9 - 80
-	request("cmd0=PutMasterVolumeSet%2F$scaledVal")
+    def int scaledVal = val * 0.9 - 80
+    request("cmd0=PutMasterVolumeSet%2F$scaledVal")
 }
 
 //turn on the receiver
 def on() {
-	sendEvent(name: "switch", value: 'on')
-	request('cmd0=PutZone_OnOff%2FON')
+    sendEvent(name: "switch", value: 'on')
+    request('cmd0=PutZone_OnOff%2FON')
 }
 
 //turn off the receiver
 def off() {
-	sendEvent(name: "switch", value: 'off')
-	request('cmd0=PutZone_OnOff%2FOFF')
+    sendEvent(name: "switch", value: 'off')
+    request('cmd0=PutZone_OnOff%2FOFF')
 }
 
 //mute the receiver
-def mute() { 
-	sendEvent(name: "mute", value: "muted")
-	request('cmd0=PutVolumeMute%2FON')
+def mute() {
+    sendEvent(name: "mute", value: "muted")
+    request('cmd0=PutVolumeMute%2FON')
 }
 
 //unmute the receiver
-def unmute() { 
-	sendEvent(name: "mute", value: "unmuted")
-	request('cmd0=PutVolumeMute%2FOFF')
+def unmute() {
+    sendEvent(name: "mute", value: "unmuted")
+    request('cmd0=PutVolumeMute%2FOFF')
 }
 
 //toggle the current mute setting
-def toggleMute(){
+def toggleMute() {
     if (device.currentValue("mute") == "muted") {
-    	unmute()
-    }
-	else {
-    	mute()
+        unmute()
+    } else {
+        mute()
     }
 }
 
 //go to the next receiver input
-def inputNext() { 
+def inputNext() {
 
-	def cur = device.currentValue("input")   
-    def selectedInputs = device.currentValue("inputChan").substring(1,device.currentValue("inputChan").length()-1).split(', ').collect{it}
+    def cur = device.currentValue("input")
+    def selectedInputs = device.currentValue("inputChan").substring(1, device.currentValue("inputChan").length() - 1).split(', ').collect {
+        it
+    }
     selectedInputs.push(selectedInputs[0])
     log.debug "SELECTED: $selectedInputs"
-    
+
     def semaphore = 0
-    for(selectedInput in selectedInputs) {
-    	if(semaphore == 1) { 
+    for (selectedInput in selectedInputs) {
+        if (semaphore == 1) {
 //        	log.debug "SELECT: ($semaphore) '$selectedInput'"
-        	return inputSelect(selectedInput)
+            return inputSelect(selectedInput)
         }
-    	if(cur == selectedInput) { 
-        	semaphore = 1
+        if (cur == selectedInput) {
+            semaphore = 1
         }
     }
 }
 
 //select a specific receiver input
 def inputSelect(channel) {
- 	sendEvent(name: "input", value: channel	)
-	request("cmd0=PutZone_InputFunction%2F$channel")
+    sendEvent(name: "input", value: channel)
+    request("cmd0=PutZone_InputFunction%2F$channel")
 }
 
-def poll() { 
-	refresh()
+def poll() {
+    refresh()
 }
 
 def refresh() {
 
     def hosthex = convertIPtoHex(destIp)
     def porthex = convertPortToHex(destPort)
-    device.deviceNetworkId = "$hosthex:$porthex" 
+    device.deviceNetworkId = "$hosthex:$porthex"
 
     def hubAction = new physicalgraph.device.HubAction(
-   	 		'method': 'GET',
-    		'path': "/goform/formMainZone_MainZoneXml.xml",
-            'headers': [ HOST: "$destIp:$destPort" ] 
-		) 
-        
+            'method': 'GET',
+            'path': "/goform/formMainZone_MainZoneXml.xml",
+            'headers': [HOST: "$destIp:$destPort"]
+    )
+
     hubAction
 }
 
-def request(body) { 
+def request(body) {
 
     def hosthex = convertIPtoHex(destIp)
     def porthex = convertPortToHex(destPort)
-    device.deviceNetworkId = "$hosthex:$porthex" 
+    device.deviceNetworkId = "$hosthex:$porthex"
 
     def hubAction = new physicalgraph.device.HubAction(
-   	 		'method': 'POST',
-    		'path': "/MainZone/index.put.asp",
-        	'body': body,
-        	'headers': [ HOST: "$destIp:$destPort" ]
-		) 
-              
+            'method': 'POST',
+            'path': "/MainZone/index.put.asp",
+            'body': body,
+            'headers': [HOST: "$destIp:$destPort"]
+    )
+
     hubAction
 }
 
 
-private String convertIPtoHex(ipAddress) { 
-    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02X', it.toInteger() ) }.join()
+private String convertIPtoHex(ipAddress) {
+    String hex = ipAddress.tokenize('.').collect { String.format('%02X', it.toInteger()) }.join()
     return hex
 }
 
 private String convertPortToHex(port) {
-	String hexport = port.toString().format( '%04X', port.toInteger() )
+    String hexport = port.toString().format('%04X', port.toInteger())
     return hexport
 }
